7d146ba439ab2a492e93cb700303ea9eb3c62520
AlexHeinisch <6390741+AlexHeinisch@users.noreply.github.com>
2023-10-13 17:44:16 +0200

    feat: rudamentary vcf creation is done

---
+++ main.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+
+def get_vcard_string(surname, name, middle_name, title, email, tel,
+                     street, zip, city, state, country) -> str:
+    if pd.isna(middle_name):
+        middle_name = ''
+    result = \
+f"""BEGIN:VCARD
+VERSION:3.0
+N:{name};{surname};{middle_name};{title};
+TEL;TYPE=home,voice;VALUE=uri:tel:{tel}
+ADR;TYPE=home;LABEL="{street}\n{zip} {city}\n{state} {country}":;;{street};{city};{state};{zip};{country}"""
+
+    if not pd.isna(email):
+        result += f'\nEMAIL;PREF;INTERNET:{email}'
+
+
+    result += """
+END:VCARD
+"""
+    return result
+
+if __name__ == '__main__':
+    output_file = './contacts.vcf'
+    input_file = 'input_files/scoreg.xlsx'
+    df = pd.read_excel(input_file)
+    file = ''
+    for idx, row in df.iterrows():
+        surname = row['Vorname']
+        name = row['Nachname']
+        middle_name = row['Zweiter Name']
+        street = row['Strasse']
+        city = row['Stadt']
+        zip = row['PLZ']
+        country = row['Land']
+        state = row['Bundesland']
+        mother_name = row['Kontakt']
+        mother_tel = row['Kontakt Telefon']
+        mother_email = row['Kontakt E-Mail']
+        father_name = row['Kontakt.1']
+        father_tel = row['Kontakt Telefon.1']
+        father_email = row['Kontakt E-Mail.1']
+
+        if not (mother_name is np.nan or mother_tel is np.nan):
+            file += get_vcard_string(title='(Mutter)', surname=surname, name=name, middle_name=middle_name, email=mother_email, 
+                                   tel=mother_tel, street=street, zip=zip, country=country, state=state, city=city)
+        if not (father_name is np.nan or father_tel is np.nan):
+            file += get_vcard_string(title='(Vater)', surname=surname, name=name, middle_name=middle_name, email=father_email, 
+                                   tel=father_tel, street=street, zip=zip, country=country, state=state, city=city)
+    with open(output_file, 'w+') as fp:
+        fp.write(file)
