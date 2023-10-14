428938336a554bd3638cf63e42def25fae8ad2f9
AlexHeinisch <6390741+AlexHeinisch@users.noreply.github.com>
2023-10-14 15:19:32 +0200

    fix: added missing type annotations for BaseSettings to be working

--- config.py
+++ config.py
@@ -1,22 +1,22 @@
 from pydantic_settings import BaseSettings
 
 class Config(BaseSettings):
-    mother_prefix = '(Mutter)'
-    father_prefix = '(Vater)'
+    mother_prefix: str = '(Mutter)'
+    father_prefix: str = '(Vater)'
 
-    column_child_name = 'Nachname'
-    column_child_surname = 'Vorname'
-    column_child_middlename = 'Zweiter Name'
-    column_street = 'Strasse'
-    column_zip = 'PLZ'
-    column_city = 'Stadt'
-    column_country = 'Land'
-    column_state = 'Bundesland'
-    column_mother_name = 'Kontakt'
-    column_father_name = 'Kontakt.1'
-    column_mother_tel = 'Kontakt Telefon'
-    column_father_tel = 'Kontakt Telefon.1'
-    column_mother_mail = 'Kontakt E-Mail'
-    column_father_mail = 'Kontakt E-Mail.1'
+    column_child_name: str = 'Nachname'
+    column_child_surname: str = 'Vorname'
+    column_child_middlename: str = 'Zweiter Name'
+    column_street: str = 'Strasse'
+    column_zip: str = 'PLZ'
+    column_city: str = 'Stadt'
+    column_country: str = 'Land'
+    column_state: str = 'Bundesland'
+    column_mother_name: str = 'Kontakt'
+    column_father_name: str = 'Kontakt.1'
+    column_mother_tel: str = 'Kontakt Telefon'
+    column_father_tel: str = 'Kontakt Telefon.1'
+    column_mother_mail: str = 'Kontakt E-Mail'
+    column_father_mail: str = 'Kontakt E-Mail.1'
 
 settings = Config()
