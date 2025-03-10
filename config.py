a357ca862be74a922bc8c21a0142558275217e59
AlexHeinisch <6390741+AlexHeinisch@users.noreply.github.com>
2025-03-10 11:20:40 +0100

    Simplify program

--- config.py
+++ config.py
@@ -6,17 +6,16 @@ class Config(BaseSettings):
 
     column_child_name: str = 'Nachname'
     column_child_surname: str = 'Vorname'
-    column_child_middlename: str = 'Zweiter Name'
     column_street: str = 'Strasse'
     column_zip: str = 'PLZ'
     column_city: str = 'Stadt'
     column_country: str = 'Land'
     column_state: str = 'Bundesland'
-    column_mother_name: str = 'Kontakt'
-    column_father_name: str = 'Kontakt.1'
-    column_mother_tel: str = 'Kontakt Telefon'
-    column_father_tel: str = 'Kontakt Telefon.1'
-    column_mother_mail: str = 'Kontakt E-Mail'
-    column_father_mail: str = 'Kontakt E-Mail.1'
+    column_mother_name: str = 'Kontakt 1'
+    column_father_name: str = 'Kontakt 2'
+    column_mother_tel: str = 'Kontakt 1 Telefon'
+    column_father_tel: str = 'Kontakt 2 Telefon'
+    column_mother_mail: str = 'Kontakt 1 Email'
+    column_father_mail: str = 'Kontakt 2 Email'
 
 settings = Config()
--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import numpy as np
 import argparse
 from config import settings
 
-def get_vcard_string(surname, name, middle_name, title, email, tel,
-                     street, zip, city, state, country) -> str:
-    if pd.isna(middle_name):
-        middle_name = ''
+def get_vcard_string(title, surname, name, email, tel) -> str:
+    middle_name = ''
     email_str = '' if pd.isna(email) else f'EMAIL;PREF;INTERNET:{email}\n'
 
     return f"""BEGIN:VCARD
 VERSION:3.0
 N:{name};{surname};{middle_name};{title};
 TEL;TYPE=home,voice;VALUE=uri:tel:{tel}
-ADR;TYPE=home;LABEL="{street}\n{zip} {city}\n{state} {country}":;;{street};{city};{state};{zip};{country}
+ADR;TYPE=home;LABEL=""
 {email_str}END:VCARD
 """
 
@@ -30,20 +28,12 @@ def process_excel(path) -> str:
             file_content += get_vcard_string(title=settings.mother_prefix, 
                                      surname=row[settings.column_child_surname],
                                      name=row[settings.column_child_name], 
-                                     middle_name=row[settings.column_child_middlename], 
-                                     email=row[settings.column_mother_mail], tel=mother_tel,
-                                     street=row[settings.column_street], zip=row[settings.column_zip],
-                                     country=row[settings.column_country], 
-                                     state=row[settings.column_state], city=row[settings.column_city])
+                                     email=row[settings.column_mother_mail], tel=mother_tel)
         if not (father_name is np.nan or father_tel is np.nan):
             file_content += get_vcard_string(title=settings.father_prefix, 
                                      surname=row[settings.column_child_surname],
                                      name=row[settings.column_child_name], 
-                                     middle_name=row[settings.column_child_middlename], 
-                                     email=row[settings.column_father_mail], tel=father_tel,
-                                     street=row[settings.column_street], zip=row[settings.column_zip],
-                                     country=row[settings.column_country], 
-                                     state=row[settings.column_state], city=row[settings.column_city])
+                                     email=row[settings.column_father_mail], tel=father_tel)
     return file_content
 
 
